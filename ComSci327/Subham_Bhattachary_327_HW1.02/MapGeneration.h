acfd8ddcf59730e16eeae11a54e74562b649c8c1
Shubham <bhattacharya.shubham123@gmail.com>
2023-08-15 22:26:14 -0500

    del

--- ComSci327/Subham_Bhattachary_327_HW1.02/MapGeneration.h
+++ ComSci327/Subham_Bhattachary_327_HW1.02/MapGeneration.h
@@ -2,7 +2,43 @@
 // Created by Subham Bhattacharya on 2/7/23.
 //
 
-#ifndef SUBHAM_BHATTACHARY_327_HW1_02_MAPGENERATION_H
-#define SUBHAM_BHATTACHARY_327_HW1_02_MAPGENERATION_H
+#ifndef MAPGENERATION_H
+#define MAPGENERATION_H
 
-#endif //SUBHAM_BHATTACHARY_327_HW1_02_MAPGENERATION_H
+#endif //MAPGENERATION_H
+
+/**
+ * Structs are like objects in C, I will create a struct called world which will be a 2d array of maps
+ * The below struct is a single node or a single map in my world
+ */
+
+typedef struct World_Node {   // Structure declaration
+    int x; // this is the x cordinate
+    int y; // this is the y cordinate
+
+    int playerX;
+    int playerY;
+
+    /**
+     * Below are the cordinates for the 4 exits
+     */
+
+    int northExit;
+    int southExit;
+    int eastExit;
+    int westExit;
+    char map[21][80];
+
+} World_Node_t;
+
+typedef struct World {
+    // current x and y coordinates the map is in
+    int x, y;
+    World_Node_t *world[401][401];
+} World_t;
+
+
+
+
+
+
--- ComSci327/Subham_Bhattachary_327_HW1.02/README.md
+++ ComSci327/Subham_Bhattachary_327_HW1.02/README.md
@@ -1,1 +1,4 @@
-This is first part of my project I have been getting the correct output and I have created a map
+This is Second part of my project I have been getting the I have created a 2 structs and have
+attempted to create helper functions to generate my world howwvr
+was unsecussfull due to lack of career fair and ab time managaement
+on my part
